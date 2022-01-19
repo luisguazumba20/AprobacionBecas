dfd1aa5cb2e63f98f83de3eb829556d9bff65730
luisguazumba20 <luis-alf20@hotmail.com>
2022-01-18 21:13:43 -0500

    Actualizado

---
+++ Consola/Program.cs
@@ -0,0 +1,20 @@
+﻿using Modelo.Entidades;
+using System;
+using System.Collections.Generic;
+
+namespace Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solicitud NuevaSolicitud = new Solicitud()
+            {
+                Admitidos = "Juanito Maldonado",
+                Becados = "Antonela Caicedo",
+                Espera = "Gustavo Loor",
+                Lista = new List<Estudiante>() { new Estudiante() { } }
+            };
+        }
+    }
+}
---
+++ ModeloDB/AprobacionBecaDB.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Modelo.Entidades;
+using System;
+
+namespace ModeloDB
+{
+    public class AprobacionBecaDB : DbContext
+    {
+        //Declaración de las entididades de Modelo
+        public DbSet<Beca> Becas { get; set; }
+        public DbSet<DatosAcademicos> Datos { get; set; }
+        public DbSet<Solicitud> Solicitudes { get; set; }
+        public DbSet<Estudiante> Estudiantes { get; set; }
+
+        //Configuración de la Conexión.
+        override protected void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(
+            "Server = DESKTOP-IQ678RP\\SQLEXPRESS; initial catalog=AprobacionBecas; trusted_connection=true;"
+            );
+        }
+    }
+}
